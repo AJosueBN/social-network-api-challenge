0e824c3c98576a4db2173c5d95b505c02e70b982
AJosueBN <129113539+AJosueBN@users.noreply.github.com>
2023-08-07 23:50:21 +0100

    ‘

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-// Schema to create a course model
+// Schema to create a Thought model schema
 const thoughtSchema = new Schema(
   {
     thoughtText: {
